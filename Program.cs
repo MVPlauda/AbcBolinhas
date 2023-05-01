d9d5294b199bd0ebd274b9b11aee0040f3577f5d
MVPlauda <plaudadev@gmail.com>
2023-05-01 18:34:59 -0300

    AbcBolinhas

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string nome;
+        double salarioAtual, novoSalario;
+
+        Console.Write("Digite o nome do funcionário: ");
+        nome = (Console.ReadLine());
+        Console.Write("Digite o salário atual do funcionário: ");
+        salarioAtual = double.Parse(Console.ReadLine());
+
+        // calcula o novo salário com aumento de 25%
+        novoSalario = salarioAtual * 1.25;
+
+        // exibe o nome do funcionário e o novo salário
+        Console.WriteLine($"O novo salário de {nome} é: {novoSalario}");
+    }
+}
